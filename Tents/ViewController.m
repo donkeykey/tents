ea2b120d3836750cf4467f7595b79355432387a8
yokotanaohiko <iam4570@yahoo.co.jp>
2014-07-26 23:17:43 +0900

    スライドで画面遷移をするようにした

--- Tents/ViewController.m
+++ Tents/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SecondViewController.h"
 #import "FunctionManager.h"
 
 @interface ViewController ()
@@ -27,6 +28,7 @@
     self.navigationController.navigationBarHidden = YES;
     _functionManager = [[FunctionManager alloc]init];
     _isLightON = NO;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,24 +63,47 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
+    NSLog(@"touch began");
     UITouch *touch = [touches anyObject];
     
     if (touch.view.tag == 5) {
-        _tentButtonPosition = [touch locationInView:self.view];
+        _tentButtonPosition = [self.view viewWithTag:5].frame.origin;
     }
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     UITouch *touch = [touches anyObject];
+    NSLog(@"touch move tag=%d",(int)touch.view.tag);
     if (touch.view.tag == 5) {
         UIView *button = [self.view viewWithTag:5];
         CGPoint pt = [touch locationInView:self.view];
-        
-        [button setFrame:CGRectMake(pt.x,button.frame.origin.y, button.frame.size.width, button.frame.size.height)];
-        
-        _tentButtonPosition = pt;
-        
+        NSLog(@"差:%f",pt.x);
+        if (_tentButtonPosition.x >= 40 && _tentButtonPosition.x <= 229 && pt.x -40 >=0 && 229-pt.x >0) {
+            [UIView animateWithDuration:0.1f animations:^{
+                [button setFrame:CGRectMake(pt.x,button.frame.origin.y, button.frame.size.width, button.frame.size.height)];
+                
+            }completion:^(BOOL finished){
+                
+            }];
+            
+        }
+        if (pt.x > 220) {
+            SecondViewController *svc = [self.storyboard instantiateViewControllerWithIdentifier:@"SecondViewController"];
+            [self.navigationController pushViewController:svc animated:YES];
+        }
     }
+}
+
+-(void)viewDidDisappear:(BOOL)animated
+{
+    UIView *button = [self.view viewWithTag:5];
+    [button setFrame:CGRectMake(40,button.frame.origin.y, button.frame.size.width, button.frame.size.height)];
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
 }
 @end
